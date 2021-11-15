fc9fb0dc9850b1f950d515de6bbda6f3a61f08f6
eddieshan <6906040+eddieshan@users.noreply.github.com>
2021-11-15 17:30:41 +0100

    Remove std:: prefix from size_t declararations

--- src/gap_buffer.h
+++ src/gap_buffer.h
@@ -7,17 +7,17 @@
 
 namespace {
     constexpr size_t Kb = 1024;
-    constexpr std::size_t BlockSize = 10*Kb;
-    constexpr std::size_t BufferSize = 3*BlockSize;
-    constexpr std::size_t BufferLimit = BufferSize - 1;
+    constexpr size_t BlockSize = 10*Kb;
+    constexpr size_t BufferSize = 3*BlockSize;
+    constexpr size_t BufferLimit = BufferSize - 1;
 }
 
 class GapBuffer {
     private:
 
         std::unique_ptr<uint8_t[]> _bytes;
-        std::size_t _gap;
-        std::size_t _end;
+        size_t _gap;
+        size_t _end;
 
         template <typename T>
         void accept(size_t from, size_t to, void (T::*write)(const uint8_t*, size_t), T& v) {
