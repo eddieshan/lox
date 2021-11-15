2cbd3ff2ede439aa098077d4beffbb2e85b9eb69
eddieshan <6906040+eddieshan@users.noreply.github.com>
2021-11-15 17:05:14 +0100

    Gap buffer with generic visitor for screen buffering

--- src/gap_buffer.cpp
+++ src/gap_buffer.cpp
@@ -1,12 +1,8 @@
+#include <cstddef>
 #include <cstdint>
 #include <memory>
 
 #include "gap_buffer.h"
-
-constexpr std::size_t Kb = 1024;
-constexpr std::size_t BlockSize = 10*Kb;
-constexpr std::size_t BufferSize = 3*BlockSize;
-constexpr std::size_t BufferLimit = BufferSize - 1;
 
 GapBuffer::GapBuffer() {
     _bytes = std::make_unique<uint8_t[]>(BufferSize);
@@ -17,16 +13,4 @@ GapBuffer::GapBuffer() {
 void GapBuffer::insert(uint8_t byte) {
     _bytes[_gap] = byte;
     _gap += 1;
-}
-
-void GapBuffer::visit(size_t from, size_t to, void (*f)(const void*, size_t))  {
-    auto start = _bytes.get() + from;
-    f(start, to - from + 1);
-}
-
-void GapBuffer::visit(void (*f)(const void*, size_t))  {
-    visit(0, _gap, f);
-    if(_end < BufferLimit) {
-        visit(_end + 1, BufferSize, f);
-    }
 }
