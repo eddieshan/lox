b045288fdfeeceb4b40e0ad027072772e57b7023
eddieshan <6906040+eddieshan@users.noreply.github.com>
2021-11-18 16:21:38 +0100

    New Cursor component

---
+++ src/cursor.cpp
@@ -0,0 +1,41 @@
+#include "term.h"
+#include "cursor.h"
+
+Cursor::Cursor(const term::WindowSize window_size):
+    _window_size(window_size),
+    _pos(0) {}
+
+unsigned int Cursor::pos() {
+    return _pos;
+}
+
+term::ScreenPosition Cursor::screen_pos() {
+    return term::ScreenPosition {
+        row: _pos / _window_size.cols,
+        col: _pos % _window_size.cols
+    };
+}
+
+void Cursor::left() {
+    if(_pos > 0) {
+        _pos--;
+    }
+}
+
+void Cursor::right() {
+    if(_pos < _window_size.rows *_window_size.cols) {
+        _pos++;
+    }
+}
+
+void Cursor::up() {
+    if(_pos >= _window_size.cols) {
+        _pos -= _window_size.cols;
+    }
+}
+
+void Cursor::down() {
+    if(_pos <= _window_size.rows *(_window_size.cols - 1)) {
+        _pos += _window_size.cols;
+    }
+}
