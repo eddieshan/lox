742951966f65e84fc1711b0bb1011716a9fb26f4
eddieshan <6906040+eddieshan@users.noreply.github.com>
2021-11-18 16:21:06 +0100

    Helper to generate VT100 escape to set pos

---
+++ src/ansi.cpp
@@ -0,0 +1,24 @@
+#include <array>
+#include <charconv>
+
+#include "ansi.h"
+#include "term.h"
+
+// The conversion of row and col to char has unknown size at compile time.
+// To avoid heap allocations, the converted rows and col are padded to three digits.
+// Thus, the VT100 escape to set cursor pos is aligned to a fixed length of 10 bytes.
+// This works based on two assumptions,
+// - that rows and cols will never be greater than 999 each,
+// - empty chars in an VT100 sequence are ignored.
+// TODO: 
+//  Returning the escape array by value is not very efficient.
+//  Find out if there is a better implementation. 
+std::array<uint8_t, 10> term::ansi::go_to(const ScreenPosition& screen_pos) {
+    auto pos = utils::array_of<uint8_t>(27, 91, 0, 0, 0, 59, 0, 0, 0, 72); // x1b[row:colH
+    const auto row_start = pos.data() + 3, col_start = pos.data() + 6;
+ 
+    std::to_chars((char*)row_start, (char*)row_start + 2, screen_pos.row);
+    std::to_chars((char*)col_start, (char*)col_start + 2, screen_pos.col);
+
+    return pos;
+}
