2c92d4b9075a172570fc2ec5e39a93309933094f
eddieshan <6906040+eddieshan@users.noreply.github.com>
2021-12-04 19:51:43 +0100

    -Fix incorrect namespace

--- src/text/navigation.h
+++ src/text/navigation.h
@@ -10,7 +10,7 @@
 // for a navigation action on a text buffer. 
 // The input position parameter is required to be a valid index within the slice.
 // Boundary check is not guaranteed, the caller is responsible for passing a valid position value.
-namespace components::navigation {
+namespace text::navigation {
 
     utils::Position screen_position(const utils::Slice<uint8_t>& text, const size_t pos);
 
