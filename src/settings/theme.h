1704da4920600b597ded4fa9ad47385f5e738df3
eddieshan <6906040+eddieshan@users.noreply.github.com>
2021-12-17 20:18:02 +0100

    Color tuning

--- src/settings/theme.h
+++ src/settings/theme.h
@@ -5,14 +5,14 @@
 
 namespace {
     constexpr auto ForegroundCode = utils::array::from<char>('2', '5', '4');
-    constexpr auto BackgroundCode = utils::array::from<char>('2', '3', '3');
+    constexpr auto BackgroundCode = utils::array::from<char>('2', '3', '5');
 }
 
 namespace settings::theme {
     const auto Foreground = term::ansi::foreground(ForegroundCode);
     const auto Background = term::ansi::background(BackgroundCode);
 
-    constexpr auto Plain = term::ansi::escape("38;5;254m");
-    constexpr auto Keyword = term::ansi::escape("38;5;169m");
-    constexpr auto TypeKeyword = term::ansi::escape("38;5;35m");
+    constexpr auto Plain = term::ansi::escape("38;5;87m");
+    constexpr auto Keyword = term::ansi::escape("38;5;174m");
+    constexpr auto TypeKeyword = term::ansi::escape("38;5;75m");
 }
