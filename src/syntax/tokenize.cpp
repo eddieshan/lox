da4e50ba5f59a2591eb565a32c091f6dcc7ab733
eddieshan <6906040+eddieshan@users.noreply.github.com>
2021-12-20 17:06:57 +0100

    Separate grammar load from tokenization

--- src/syntax/tokenize.cpp
+++ src/syntax/tokenize.cpp
@@ -1,4 +1,3 @@
-#include <memory>
 #include <cstring>
 
 #include "../utils/slice.h"
@@ -9,36 +8,6 @@
 using namespace utils;
 using namespace syntax;
 
-constexpr auto TokenDelimiter = '|';
-
-TokenGroup syntax::tokens(const char* tokens_def, const TokenType token_type) {
-
-    const auto tokens_length = strlen(tokens_def) + 1;
-
-    auto token_group = TokenGroup {
-        tokens: std::make_unique<uint8_t[]>(tokens_length),
-        size: tokens_length,
-        type: token_type
-    };
-
-    const auto tokens = token_group.tokens.get();
-    auto index = 0;
-
-    for(auto i = 0; i < tokens_length; ++i) {
-        if(tokens_def[i] == TokenDelimiter || tokens_def[i] == '\0') {
-            const auto offset = index == 0? index : index + 1;
-            const auto token_size = i - offset;
-
-            tokens[offset] = token_size;
-            std::copy(tokens_def + offset, tokens_def + i, tokens + offset + 1);
-
-            index = i;
-        }
-    }
-
-    return token_group;
-}
-
 TokenType token_type(const Slice<uint8_t>& sequence, const Grammar& grammar) {
 
     for(auto i = 0; i < grammar.tokens.size; ++i) {
@@ -48,10 +17,10 @@ TokenType token_type(const Slice<uint8_t>& sequence, const Grammar& grammar) {
 
         while(length_index < grammar.tokens.data[i].size) {
             const auto token_size = tokens[length_index];
-            const auto token_index = length_index + 1;
+            const auto token_start = tokens + length_index + 1;
 
             if(token_size == sequence.size) {
-                if(std::memcmp(tokens + token_index, sequence.data, token_size) == 0) {
+                if(std::memcmp(token_start, sequence.data, token_size) == 0) {
                     return grammar.tokens.data[i].type;
                 }
             }
