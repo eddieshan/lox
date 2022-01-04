6e8e974d4da5384254eaccc21b18a2bd5a9e95ba
eddieshan <6906040+eddieshan@users.noreply.github.com>
2022-01-04 19:08:34 +0100

    Slice and Range generic helper functions

--- src/utils/range.cpp
+++
@@ -1,8 +0,0 @@
-#include <cstdint>
-#include "range.h"
-
-using namespace utils;
-
-bool range::contains(const Range& range, const uint8_t val) {
-    return val >= range.start && val <= range.end;
-}
--- src/utils/slice.h
+++ src/utils/slice.h
@@ -3,24 +3,67 @@
 #include <cstdint>
 #include <array>
 
+#include "range.h"
+
 namespace utils {
 
     // Immutable, non owned slice type.
     // It is meant to be used for sharing read only slices.
-    template<typename T>
+    template<typename TItem>
     struct Slice {
-        T* data;
+        TItem* data;
         size_t size;
 
-        Slice(T* data_, const size_t size_): data(data_), size(size_) {}
+        Slice(TItem* data_, const size_t size_): data(data_), size(size_) {}
     };
 
     namespace slice {
 
         Slice<uint8_t> from(const char* val);
 
-        template<typename T>
-        bool contains(const Slice<T>& slice, const T val) {
+        template<typename TItem>
+        Slice<TItem> sub(const Slice<TItem>& slice, const Range<size_t>& range) {
+            return Slice(slice.data + range.start, range.end - range.start + 1);
+        }        
+
+        template<typename TItem>
+        size_t count(const Slice<TItem>& slice, const TItem val) {
+            auto count = 0;
+            for(auto i = 0; i < slice.size; ++i) {
+                if(slice.data[i] == val) {
+                    ++count;
+                }
+            }
+
+            return count;
+        }
+
+        template<typename TItem>
+        size_t count(const Slice<TItem>& slice, const TItem val, const size_t from, const size_t to) {
+            auto count = 0;
+            for(auto i = from; i <= to; ++i) {
+                if(slice.data[i] == val) {
+                    ++count;
+                }
+            }
+
+            return count;
+        }
+
+        template<typename TItem>
+        size_t count(const Slice<TItem>& slice, const TItem val, const Range<size_t>& range) {
+            auto count = 0;
+            for(auto i = range.start; i <= range.end; ++i) {
+                if(slice.data[i] == val) {
+                    ++count;
+                }
+            }
+
+            return count;
+        }
+
+        template<typename TItem>
+        bool contains(const Slice<TItem>& slice, const TItem val) {
             for(auto i = 0; i < slice.size; ++i) {
                 if(slice.data[i] == val) {
                     return true;
@@ -28,6 +71,106 @@ namespace utils {
             }
 
             return false;
+        }
+
+        template<typename TItem, bool (*predicate)(const TItem val)>
+        size_t find(const Slice<TItem>& slice) {
+            for(auto i = 0; i < slice.size; ++i) {
+                if(predicate(slice.data[i])) {
+                    return i;
+                }
+            }
+
+            return slice.size;
+        }
+
+        template<typename TItem, bool (*predicate)(const TItem val)>
+        size_t find(const Slice<TItem>& slice, const size_t from, const size_t to) {
+            for(auto i = from; i <= to; ++i) {
+                if(predicate(slice.data[i])) {
+                    return i;
+                }
+            }
+
+            return slice.size;
+        }
+
+        template<typename TItem>
+        size_t find(const Slice<TItem>& slice, const TItem val, const size_t from) {
+            for(auto i = from; i < slice.size; ++i) {
+                if(slice.data[i] == val) {
+                    return i;
+                }
+            }
+
+            return slice.size;
+        }
+
+        template<typename TItem>
+        size_t find(const Slice<TItem>& slice, const TItem val, const Range<size_t>& range) {
+            for(auto i = range.start; i <= range.end; ++i) {
+                if(slice.data[i] == val) {
+                    return i;
+                }
+            }
+
+            return slice.size;
+        }
+
+        template<typename TItem>
+        size_t find_n(const Slice<TItem>& slice, const TItem val, const size_t target) {
+            auto count = 0;
+            for(auto i = 0; i < slice.size; ++i) {
+                if(slice.data[i] == val) {
+                    ++count;
+                    if(count == target) {
+                        return i;
+                    }
+                }
+            }
+
+            return slice.size;
+        }
+
+        template<typename TItem>
+        size_t find_n(const Slice<TItem>& slice, const TItem val, const size_t from, const size_t target) {
+            auto count = 0;
+            for(auto i = from; i < slice.size; ++i) {
+                if(slice.data[i] == val) {
+                    ++count;
+                    if(count == target) {
+                        return i;
+                    }
+                }
+            }
+
+            return slice.size;
+        }
+
+        template<typename TItem>
+        size_t find_back(const Slice<TItem>& slice, const TItem val, const size_t from) {
+            for(size_t i = from + 1; i > 0; --i) {
+                if(slice.data[i - 1] == val) {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+        template<typename TItem>
+        size_t find_n_back(const Slice<TItem>& slice, const TItem val, const size_t from, const size_t target) {
+            auto count = 0;
+            for(size_t i = from + 1; i > 0; --i) {
+                if(slice.data[i - 1] == val) {
+                    ++count;
+                    if(count == target) {
+                        return i;
+                    }
+                }
+            }
+
+            return 0;
         }
 
     }
