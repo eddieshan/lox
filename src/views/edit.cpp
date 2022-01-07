57656b5874717b174279c3bfed2e78b94f3ef744
eddieshan <6906040+eddieshan@users.noreply.github.com>
2022-01-07 22:03:20 +0100

    Fix unrecognized tokens causing infinite loop

--- src/views/edit.cpp
+++ src/views/edit.cpp
@@ -24,8 +24,8 @@ void views::edit(const EditorState& state, const settings::Config& config, buffe
 
     const auto full_text = state.text();
     const auto clipped_text = slice::sub(full_text, visible_region);
-    //const auto start_pos = views::syntax(text, config.grammar, screen_buffer);
-    const auto start_pos = views::plain_text(clipped_text, screen_buffer);
+    const auto start_pos = views::syntax(clipped_text, config.grammar, screen_buffer);
+    //const auto start_pos = views::plain_text(clipped_text, screen_buffer);
 
     const auto start_line = slice::count(full_text, ascii::Lf, 0, visible_region.start);
     const auto end_line = slice::count(clipped_text, ascii::Lf) + start_line;
