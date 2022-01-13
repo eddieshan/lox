476fd90340468e9364ec92c1a3c5fe492cc4162c
eddieshan <6906040+eddieshan@users.noreply.github.com>
2022-01-13 19:09:15 +0100

    Complete C++ grammar keywords

--- src/syntax/cpp.h
+++ src/syntax/cpp.h
@@ -8,7 +8,13 @@ namespace syntax::cpp {
     constexpr auto CommentDelimiters = "//||/*|*/";
     constexpr auto FunctionCallDelimiters = utils::Range<uint8_t> { start: (uint8_t)'(', end: (uint8_t)')'};
 
-    constexpr auto Keywords = "return|if|else|switch|break|do|while|for|using|#include|new|delete";
-    constexpr auto TypeKeywords = "constexpr|const|auto|namespace|void|unsigned|bool|byte|int|char|double|long|enum|class|struct|template";
+    constexpr auto Keywords = 
+        "return|if|else|switch|case|break|do|while|for|continue|goto|using|"
+        "try|catch|throw|new|delete|default|"
+        "#include|#pragma|#error|#define|#undef|#if|#elif|#else|#endif|#ifdef|#ifndef";
+    constexpr auto TypeKeywords = 
+        "constexpr|const|auto|namespace|void|unsigned|bool|char|byte|short|int|float|double|long|enum|"
+        "class|virtual|final|override|struct|union|friend|inline|static|register|sizeof|this|"
+        "template|typedef|static_cast|dynamic_cast|const_cast|reinterpret_cast|extern|asm";
     constexpr auto Operators = "+-*/%<>=[]&|~?:";
 }
